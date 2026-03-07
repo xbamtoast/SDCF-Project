f1686ba7269fe788312d62fa287ef185ce594b2c
REGINALD COLE <rc0551329@otc.edu>
2026-03-07 15:16:54 -0600

    removed admin_agreement.html. I dont think itll be necessary

--- Django/app/urls.py
+++ Django/app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('admin_agreement/', views.admin_agreement, name='admin_agreement'),
     path('recipient_agreement/', views.recipient_agreement, name='recipient_agreement'),
     path('w9_upload/', views.w9_upload, name='w9_upload'),
 ]
--- Django/app/views.py
+++ Django/app/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def admin_agreement(request): #view to render admin agreement
-    return render(request, 'admin_agreement.html')
 
 def recipient_agreement(request): #view to render recipient agreement
     return render(request, 'recipient_agreement.html')
