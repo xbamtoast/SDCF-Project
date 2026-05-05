197186627b84c208cb8be4be4e47b85dc03d4ea3
goochmawn314 <goochmawn314@gmail.com>
2026-05-05 18:49:45 -0500

    Upload Procfile.

--- Django/mainsite/settings.py
+++ Django/mainsite/settings.py
@@ -14,19 +14,20 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'django-insecure-a0jk!v^buu0(8n_03&y6m#9ir%fm^)!8ilvqla%y_3b1z-g5bg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.railway.app']
 
 # Application definition
 
